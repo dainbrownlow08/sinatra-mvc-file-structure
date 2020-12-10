caf3dc81409156e98d2665cf869e9da99ce6ebce
dainbrownlow08 <dain.brownlow@outlook.com>
2020-12-09 17:12:21 -0800

    done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
-class Model
+class Dog
     attr_accessor :name, :age, :breed
     @@all = []
-    def initialize(name:,breed:,age:)
+    def initialize(name,breed,age)
         @name = name
         @breed = breed
         @age = age
